d8ae6b5b14def53c4a19a8fea95fb6f60744d2d3
gustavjlindqvist <gurra.lindqvist@gmail.com>
2017-02-05 22:23:29 +0100

    Update Huffman.hs

--- Huffman.hs
+++ Huffman.hs
@@ -105,7 +105,7 @@ huffmanTree table =
       {- addToQueue q x@(a, b)
          PRE:       b > 0.
          POST:      Inserts x in queue q.
-         EXAMPLES:  addToQueue PriorityQueue.empty ('a', 2) == PriorityQueue [(Leaf 2 'a', 2)] <--------Gustav ändrade tillbaka hehe
+         EXAMPLES:  addToQueue PriorityQueue.empty ('a', 2) == PriorityQueue [(Leaf 2 'a', 2)]
       -}
       addToQueue :: PriorityQueue HuffmanTree -> (Char, Int) -> PriorityQueue HuffmanTree
       addToQueue q (c, i) = PriorityQueue.insert q ((Leaf i c), i)
